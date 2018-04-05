66496c4a3e6a464d829a978391208cffac3912ba
Petr Pridal <petr.pridal@klokantech.com>
2018-04-05 17:02:33 +0200

    Updated Evropa: asdf serie

--- config.js
+++ config.js
@@ -93,6 +93,20 @@ mapseries.config = {
           return stringValue;
         }
       }
+    },
+    {
+      title: "Evropa: asdf",
+      layer: "evropa-asdf",
+      template: "evropa-asdf.txt",
+      formatFunctions: {
+        addDegrees: function (stringValue, sheet) {
+          stringValue += '';
+          if(stringValue.length==4) {
+            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
+          }
+          return stringValue;
+        }
+      }
     }
   ]
 }
